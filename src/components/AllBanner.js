1cba42307e8f03456d15546b7ec4cb9202c320e4
HN <rkdgosk1653@naver.com>
2023-04-24 12:10:13 +0900

    again send cause not store

---
+++ src/components/AllBanner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AllBanner = () => {
+    return (
+    <container className="AllBanner">
+    <div>
+    <img src="상단배너이미지주소" alt="이미지 준비 중" />
+    </div>
+    </container>
+    );
+    };
+    
---
+++ src/components/AllFilterButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AllFilterButton = () => {
+    return (
+    <div className="AllFilterButton">
+    <FilterButton text={"All"} />
+    <FilterButton text={"영양제"} />
+    <FilterButton text={"운동복"} />
+    <FilterButton text={"운동기구"} />
+    </div>
+    );
+    };
