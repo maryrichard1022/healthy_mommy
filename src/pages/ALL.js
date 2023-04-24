026e4a2324dab048d9e855dd18166f84ee83b574
HN <rkdgosk1653@naver.com>
2023-04-24 12:08:32 +0900

    ALL AllBanner AllFilterButton create

--- src/pages/ALL.js
+++ src/pages/ALL.js
@@ -1,1 +1,25 @@
 //모든 물품 띄우는 페이지
+import React from "react";
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+
+import AllBanner from "../components/AllBanner";
+import AllFilterButton from "../components/AllFilterButton";
+
+import "./ALL.css";
+
+function ALL () {
+    return (
+      <div className="all">
+        <div className="contentWrapper">
+          <Nav />
+          <AllBanner />
+	        <AllFilterButton />
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+  
+  export default ALL;
+  
